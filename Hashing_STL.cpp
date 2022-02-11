83b456ccd03db9ff61cd0c05da282c1af23ec8d1
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-11 21:49:08 +0530

    Add files via upload

---
+++ Hashing_STL.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <set>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+
+int main()
+{
+    map<int, int> m;
+    m[8] = 2;
+    cout << m[8] << endl;
+
+    return 0;
+}
