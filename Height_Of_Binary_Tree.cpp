4000f47141c2c9f5520dfd4ae9daa0c5850d48c3
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-04-10 17:38:07 +0530

    Add files via upload

---
+++ Height_Of_Binary_Tree.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+void printLevelOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            cout << node->data << " ";
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+            q.push(NULL);
+    }
+}
+
+int sumAtK(Node *root, int K)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    int level = 0;
+    int sum = 0;
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            if (level == K)
+                sum += node->data;
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+            level++;
+        }
+    }
+    return sum;
+}
+
+int countNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return countNodes(root->left) + countNodes(root->right) + 1;
+}
+
+int sumNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return sumNodes(root->left) + sumNodes(root->right) + root->data;
+}
+
+int calcHeight(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lHeight = calcHeight(root->left);
+    int rHeight = calcHeight(root->right);
+    return max(lHeight, rHeight) + 1;
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout << "The height of binary tree is : ";
+    cout << calcHeight(root) << endl;
+    return 0;
+}
