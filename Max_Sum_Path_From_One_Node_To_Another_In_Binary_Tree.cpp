e8b0750abf8b64e5f04337efacd4d1621a396a7c
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-04-12 23:23:40 +0530

    Add files via upload

---
+++ Max_Sum_Path_From_One_Node_To_Another_In_Binary_Tree.cpp
@@ -0,0 +1,373 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+void printLevelOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            cout << node->data << " ";
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+            q.push(NULL);
+    }
+}
+
+int sumAtK(Node *root, int K)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    int level = 0;
+    int sum = 0;
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            if (level == K)
+                sum += node->data;
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+            level++;
+        }
+    }
+    return sum;
+}
+
+int countNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return countNodes(root->left) + countNodes(root->right) + 1;
+}
+
+int sumNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return sumNodes(root->left) + sumNodes(root->right) + root->data;
+}
+
+int calcHeight(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lHeight = calcHeight(root->left);
+    int rHeight = calcHeight(root->right);
+    return max(lHeight, rHeight) + 1;
+}
+
+int calcDiameter(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lHeight = calcHeight(root->left);
+    int rHeight = calcHeight(root->right);
+    int currDiameter = lHeight + rHeight + 1;
+    int lDiameter = calcDiameter(root->left);
+    int rDiameter = calcDiameter(root->right);
+    return max(currDiameter, max(lDiameter, rDiameter));
+}
+
+void sumReplace(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    sumReplace(root->left);
+    sumReplace(root->right);
+    if (root->left != NULL)
+    {
+        root->data += root->left->data;
+    }
+    if (root->right != NULL)
+    {
+        root->data += root->right->data;
+    }
+}
+
+bool isBalanced(Node *root)
+{
+    if (root == NULL)
+    {
+        return true;
+    }
+    if (isBalanced(root->left) == false)
+    {
+        return false;
+    }
+    if (isBalanced(root->right) == false)
+    {
+        return false;
+    }
+    int lh = calcHeight(root->left);
+    int rh = calcHeight(root->right);
+    if (abs(lh - rh) <= 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void rightView(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+            if (i == n - 1)
+            {
+                cout << curr->data << " ";
+            }
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+}
+
+void leftView(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 1; i <= n; i++)
+        {
+            Node *temp = q.front();
+            q.pop();
+            if (i == 1)
+            {
+                cout << temp->data << " ";
+            }
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void flatten(Node *root)
+{
+    if (root == NULL || root->left == NULL && root->right == NULL)
+    {
+        return;
+    }
+    if (root->left != NULL)
+    {
+        flatten(root->left);
+        Node *temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        Node *t = root->right;
+        while (t->right != NULL)
+        {
+            t = t->right;
+        }
+        t->right = temp;
+    }
+    flatten(root->right);
+}
+
+bool getPath(Node *root, int key, vector<int> &path)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    path.push_back(root->data);
+    if (root->data == key)
+    {
+        return true;
+    }
+    if (getPath(root->left, key, path) || getPath(root->right, key, path))
+    {
+        return true;
+    }
+    path.pop_back();
+    return false;
+}
+
+int LCA(Node *root, int n1, int n2)
+{
+    vector<int> path1, path2;
+    if (!getPath(root, n1, path1) || !getPath(root, n2, path2))
+    {
+        return -1;
+    }
+    int pc;
+    for (pc = 0; pc < path1.size() && path2.size(); pc++)
+    {
+        if (path1[pc] != path2[pc])
+        {
+            break;
+        }
+    }
+    return path1[pc - 1];
+}
+
+Node *LCA2(Node *root, int n2, int n1)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data == n1 || root->data == n2)
+    {
+        return root;
+    }
+    Node *leftLCA = LCA2(root->left, n2, n1);
+    Node *rightLCA = LCA2(root->right, n2, n1);
+    if (leftLCA && rightLCA)
+    {
+        return root;
+    }
+    if (leftLCA != NULL)
+    {
+        return leftLCA;
+    }
+    return rightLCA;
+}
+
+int maxPathSumUtil(Node *root, int &ans)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int left = maxPathSumUtil(root->left, ans);
+    int right = maxPathSumUtil(root->right, ans);
+    int nodeMax = max(max(root->data, root->data + left + right), max(root->data + left, root->data + right));
+    ans = max(ans, nodeMax);
+    int singlePathSum = max(root->data, max(root->data + left, root->data + right));
+    return singlePathSum;
+}
+
+int maxPathSum(Node *root)
+{
+    int ans = INT_MIN;
+    maxPathSumUtil(root, ans);
+    return ans;
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout << maxPathSum(root) << endl;
+    return 0;
+}
