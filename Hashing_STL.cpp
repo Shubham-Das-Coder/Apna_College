bc963af4d4b0d1934278e3672df35c57b8225075
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-04-18 18:52:00 +0530

    Add files via upload

---
+++ Hashing_STL.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+
+signed main()
+{
+    map<int, int> m;
+    m[8] = 2;
+    cout << m[8] << endl;
+    return 0;
+}
